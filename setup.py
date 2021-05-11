4247e84720c6e0d8a16676e84e2e1a9cf4b5bc15
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-05-11 21:21:06 +0800

    bumped version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="audio4t",
-    version="0.0.0",
+    version="0.0.1",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="Audio wrapper for Teenagers",
