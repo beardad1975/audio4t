ca80f7a67d556a95bb47ef8a58cf512916542e70
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2023-01-31 17:41:41 +0800

    bumped version 0.0.4 (update dep versions)

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ import setuptools
 
 setuptools.setup(
     name="audio4t",
-    version="0.0.3",
+    version="0.0.4",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
-    description="Audio wrapper for Teenagers",
-    long_description="Audio wrapper for Teenagers",
+    description="Audio learning wrapper library for Teenagers",
+    long_description="Audio learning wrapper library for Teenagers",
     long_description_content_type="text/markdown",
     url="https://github.com/beardad1975/audio4t",
     #packages=setuptools.find_packages(),
     platforms=["Windows"],
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     packages=['audio4t','聲音模組'],
-    install_requires = ['pydub == 0.25.1', 'simpleaudio == 1.0.4', 'scipy == 1.5.4'],
+    install_requires = ['pyaudio == 0.2.13','pydub == 0.25.1', 'simpleaudio == 1.0.4'],
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
